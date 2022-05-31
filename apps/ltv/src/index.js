b59c127d350a7478c4dd50424ebc999dc319faf5
fissben <rda.onell@gmail.com>
2022-05-31 13:01:31 +0300

    add new feature

--- apps/ltv/src/index.js
+++ apps/ltv/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Ltv from "./components/Ltv/Ltv";
-
+//add new feature
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
