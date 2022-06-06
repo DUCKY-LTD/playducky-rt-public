4742d2719ef9c5b35c353af763ed3e1c3a885063
Yevhenii Kucher <evhenii.kucher@gmail.com>
2022-06-06 15:30:35 +0300

    test 2(build)

--- apps/ltv/src/components/Ltv/TBody/TBody.js
+++ apps/ltv/src/components/Ltv/TBody/TBody.js
@@ -13,57 +13,27 @@ function TBody({ sortedData }) {
             <td className={styles.cell}>{el.cpi}</td>
             <td className={styles.cell}>{el.installs}</td>
             <td
-              className={styles.cell}
-              style={{
-                backgroundColor: el.d0.type === "fact" ? "#77AAD1" : "#FFFFFF",
-                color: el.d0.type === "fact" ? "#FFFFFF" : "#6E6C6C",
-              }}
-            >
+              className={styles.cell}>
               {el.d0.value}
             </td>
             <td
-              className={styles.cell}
-              style={{
-                backgroundColor: el.d3.type === "fact" ? "#77AAD1" : "#FFFFFF",
-                color: el.d3.type === "fact" ? "#FFFFFF" : "#6E6C6C",
-              }}
-            >
+              className={styles.cell}>
               {el.d3.value}
             </td>
             <td
-              className={styles.cell}
-              style={{
-                backgroundColor: el.d7.type === "fact" ? "#77AAD1" : "#FFFFFF",
-                color: el.d7.type === "fact" ? "#FFFFFF" : "#6E6C6C",
-              }}
-            >
+              className={styles.cell}>
               {el.d7.value}
             </td>
             <td
-              className={styles.cell}
-              style={{
-                backgroundColor: el.d30.type === "fact" ? "#77AAD1" : "#FFFFFF",
-                color: el.d30.type === "fact" ? "#FFFFFF" : "#6E6C6C",
-              }}
-            >
+              className={styles.cell}>
               {el.d30.value}
             </td>
             <td
-              className={styles.cell}
-              style={{
-                backgroundColor: el.d60.type === "fact" ? "#77AAD1" : "#FFFFFF",
-                color: el.d60.type === "fact" ? "#FFFFFF" : "#6E6C6C",
-              }}
-            >
+              className={styles.cell}>
               {el.d60.value}
             </td>
             <td
-              className={styles.cell}
-              style={{
-                backgroundColor: el.d90.type === "fact" ? "#77AAD1" : "#FFFFFF",
-                color: el.d90.type === "fact" ? "#FFFFFF" : "#6E6C6C",
-              }}
-            >
+              className={styles.cell}>
               {el.d90.value}
             </td>
           </tr>
