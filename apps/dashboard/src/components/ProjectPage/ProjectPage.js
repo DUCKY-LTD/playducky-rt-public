af42b29efe47194fd16b4d1c2e16f60f83ce87fd
Yevhenii Kucher <evhenii.kucher@gmail.com>
2022-07-08 16:14:41 +0300

    project test 6 (build)

--- apps/dashboard/src/components/ProjectPage/ProjectPage.js
+++ apps/dashboard/src/components/ProjectPage/ProjectPage.js
@@ -79,45 +79,47 @@ class ProjectPage extends Component {
         // console.log(this.state.teamData)
 
         return (
-            <div className={styles.container}>
+            // <div className={styles.container}>
+            //
+            //     <div className={styles.card}>
+            //         <img className={styles.img} src={gameicon_image} alt="project_icon" width={135} height={135}/>
+            //         <div>
+            //             <h1 className={styles.title}>{game_name_text}</h1>
+            //             <h2 className={styles.team}>By {team_name_text} Team</h2>
+            //             <div className={styles.cardInfo}>
+            //                 <p>Released Platforms</p>
+            //                 <p>Last update {modifyString(modifiedDate)}</p>
+            //             </div>
+            //         </div>
+            //         <div className={styles.editBlock}>
+            //             <StaffCard title={'Producer'} name={'Nick Shapovalov'}/>
+            //             <StaffCard title={'Marketing'} name={'Not assigned'}/>
+            //             <div>
+            //                 <ul style={{listStyle: "none"}}>
+            //                     <li>
+            //                         <TransitionsModal btnName={'EDIT GAME'} btnBgColor={'#ed652b'} gameName={game_name_text} type = {'edit'}
+            //                                           editGameHandler={this.editGameHandler} modalWidth={630}/>
+            //                     </li>
+            //                     <li>
+            //                         <TransitionsModal btnName={'DELETE GAME'} btnBgColor={'#d90000'} gameName={game_name_text} type={'delete'}
+            //                                           modalWidth={410}/>
+            //                     </li>
+            //                     <li>
+            //                         <a href="https://www.notion.so/playducky/61923c2f4c9846d78ea39eb61a18a1df?v=005720ff02b14720ad3450395bcfd929" target='_blank'
+            //                            rel="noopener noreferrer">
+            //                             <img src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1638797149646x236744111126793600%2FNotion_app_logo.png?w=64&h=64&auto=compress&fit=crop&dpr=1"
+            //                                  alt="notion_logo" width={45} height={45}/>
+            //                             <p>Project page</p>
+            //                         </a>
+            //                     </li>
+            //                 </ul>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     <TabsPanel ltv={ <Ltv/> } gameName={game_name_text}  experimentList={ <ExperimentList data={this.state.experimentData}/>}/>
+            // </div>
 
-                <div className={styles.card}>
-                    <img className={styles.img} src={gameicon_image} alt="project_icon" width={135} height={135}/>
-                    <div>
-                        <h1 className={styles.title}>{game_name_text}</h1>
-                        <h2 className={styles.team}>By {team_name_text} Team</h2>
-                        <div className={styles.cardInfo}>
-                            <p>Released Platforms</p>
-                            <p>Last update {modifyString(modifiedDate)}</p>
-                        </div>
-                    </div>
-                    <div className={styles.editBlock}>
-                        <StaffCard title={'Producer'} name={'Nick Shapovalov'}/>
-                        <StaffCard title={'Marketing'} name={'Not assigned'}/>
-                        <div>
-                            <ul style={{listStyle: "none"}}>
-                                <li>
-                                    <TransitionsModal btnName={'EDIT GAME'} btnBgColor={'#ed652b'} gameName={game_name_text} type = {'edit'}
-                                                      editGameHandler={this.editGameHandler} modalWidth={630}/>
-                                </li>
-                                <li>
-                                    <TransitionsModal btnName={'DELETE GAME'} btnBgColor={'#d90000'} gameName={game_name_text} type={'delete'}
-                                                      modalWidth={410}/>
-                                </li>
-                                <li>
-                                    <a href="https://www.notion.so/playducky/61923c2f4c9846d78ea39eb61a18a1df?v=005720ff02b14720ad3450395bcfd929" target='_blank'
-                                       rel="noopener noreferrer">
-                                        <img src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1638797149646x236744111126793600%2FNotion_app_logo.png?w=64&h=64&auto=compress&fit=crop&dpr=1"
-                                             alt="notion_logo" width={45} height={45}/>
-                                        <p>Project page</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <TabsPanel ltv={ <Ltv/> } gameName={game_name_text}  experimentList={ <ExperimentList data={this.state.experimentData}/>}/>
-            </div>
+            <div className={styles.test}>TEST</div>
         );
     }
 }
